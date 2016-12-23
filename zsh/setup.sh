3c3e9c75686918c52f3e282bcf2dbbeb9576eb22
Ryan <ryan.moore@salesforce.com>
2016-12-22 21:13:03 -0500

    zsh install for linux

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,6 +3,9 @@ rm ~/.zshrc
 rm -rf ~/.oh-my-zsh
 
 # install zsh
+if [ $os == "linux-gnu" ]; then
+	sudo apt-get install zsh
+fi
 
 # install oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
