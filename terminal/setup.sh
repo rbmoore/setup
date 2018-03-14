312adcbed43b9f112ce20aa81f84b72fb1f7a261
ryan.moore <ryan.moore@salesforce.com>
2018-03-14 15:24:40 -0400

    terminator config for linux

--- terminal/setup.sh
+++ terminal/setup.sh
@@ -1,6 +1,14 @@
 # install terminator
 if [ $os == "linux-gnu" ]; then
-	sudo add-apt-repository ppa:gnome-terminator
-	sudo apt-get update
-	sudo apt-get install terminator
+	if ! [ -x "$(command -v terminator)" ]; then
+		sudo add-apt-repository ppa:gnome-terminator
+		sudo apt-get update
+		sudo apt-get install terminator
+	fi
+
+	if [ -f ~/.config/terminator/config ]; then
+		rm ~/.config/terminator/config
+	fi
+
+	cp ./terminal/terminator-config ~/.config/terminator/config
 fi
